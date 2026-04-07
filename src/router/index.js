769cfa798bb608f373de4dbe9d1f6543f47dc4cf
Song TaeKwon <79988462+SongTaeKwon@users.noreply.github.com>
2026-04-07 22:24:00 +0900

    로컬 회원가입, 로그인, 로그아웃, 라우터 가드 구현
    * feat: api 요청용 axios 인스턴스 구현
    
    * docs: 유저 데이터 스키마 작성
    
    * feat: 유저 회원가입, 로그인, 로그아웃 구현
    
    * Revert "docs: 유저 데이터 스키마 작성"
    
    This reverts commit 5bc8604c947270d79839cda00019bcddfb4a55ff.
    
    * refactor: 로그인 여부 변수 분리
    
    * feat: 로그인 여부 라우트 가드 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import {useUserStore} from "@/stores/user.js";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [],
 })
+
+router.beforeEach(async (to, from, next) => {
+    const userStore = useUserStore();
+
+    if (!userStore.isLoggedIn) {
+        const loginInformation = localStorage.getItem('userId');
+        if (loginInformation) {
+            await userStore.fetchUser();
+        }
+    }
+
+    if (to.meta.requiresAuth && !userStore.isLoggedIn) {
+        return next('/login') //TODO: 로그인 페이지 구현
+    }
+
+    next();
+});
 
 export default router
---
+++ src/stores/user.js
@@ -0,0 +1,71 @@
+import {defineStore} from "pinia";
+import api from "@/plugin/api.js";
+import {computed, ref} from "vue";
+
+export const useUserStore = defineStore("user", () => {
+    const user = ref(null);
+
+    const isLoggedIn = computed(() => !!user.value);
+
+    async function register(registerData) {
+        let isUsed = true;
+        try {
+            const res = await api.get(`/users?username=${registerData.username}`);
+            isUsed = res.data.length !== 0;
+        } catch (error) {
+            throw new Error("회원가입 중 오류가 발생했습니다.");
+        }
+        if (isUsed) {
+            throw new Error("이미 사용중인 아이디입니다.");
+        }
+        try {
+            await api.post(`/users`, registerData);
+        } catch (error) {
+            throw new Error("회원가입 중 오류가 발생했습니다.");
+        }
+    }
+
+    async function login(loginData) {
+        let response;
+        try {
+            response = await api.get(`/users?username=${loginData.username}`);
+        } catch (e) {
+            throw new Error("로그인 중 오류가 발생했습니다.");
+        }
+        if (response.data.length === 0 || response.data[0]?.password !== loginData.password) {
+            throw new Error("잘못된 아이디 혹은 비밀번호 입니다.");
+        }
+        const data = response.data[0];
+        localStorage.setItem("userId", data.id);
+        setUser(data);
+    }
+
+    function logout() {
+        localStorage.removeItem("userId");
+        user.value = null;
+    }
+
+    async function fetchUser() {
+        const userId = localStorage.getItem("userId");
+        if (!userId) {
+            logout();
+            return;
+        }
+        try {
+            const res = await api.get(`/users/${userId}`);
+            const data = res.data;
+            setUser(data);
+        } catch (e) {
+            logout();
+        }
+    }
+
+    function setUser(data) {
+        user.value = {
+            id: data.id,
+            nickname: data.nickname
+        };
+    }
+
+    return {user, isLoggedIn, login, register, fetchUser, logout};
+});
