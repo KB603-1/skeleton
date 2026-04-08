916da1291f7ae2715fa178300aedb984588d2783
SongTaeKwon <taekwon0321@gmail.com>
2026-04-08 15:29:06 +0900

    feat: LoggedIn 레이아웃 분리

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import { useUserStore } from '@/stores/user.js';
-
+import {createRouter, createWebHistory} from "vue-router";
+import {useUserStore} from "@/stores/user.js";
+import ShareListPage from "@/pages/share/ShareListPage.vue";
+import MainPage from "@/page/MainPage.vue";
+import ChartPage from "@/page/ChartPage.vue";
+import AddPage from "@/page/AddPage.vue";
+import SignInPage from "@/pages/SignInPage.vue";
+import SignUpPage from "@/pages/SignUpPage.vue";
+import LoggedIn from "@/layouts/LoggedIn.vue";
 import MonthlyHistory from '@/pages/MonthlyHistory.vue';
 import EditRecord from '@/pages/EditRecord.vue';
-import MainPage from '@/pages/main/MainPage.vue';
-import ChartPage from '@/pages/chart/ChartPage.vue';
-import AddPage from '@/pages/add/AddPage.vue';
-import ShareListPage from '@/pages/share/ShareListPage.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { path: '/monthly', component: MonthlyHistory },
-    { path: '/edit', component: EditRecord },
-    { path: '/share', name: 'share', component: ShareListPage },
-    { path: '/', component: MainPage },
-    { path: '/stats', component: ChartPage },
-    { path: '/add', component: AddPage },
-    {path: '/login', component: SignInPage },
-    {path: '/signUp', component: SignUpPage },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/", component: LoggedIn, children: [
+                {path: "", component: MainPage},
+                {path: "/share", name: "share", component: ShareListPage},
+                {path: "/stats", component: ChartPage},
+                {path: "/add", component: AddPage},
+                { path: '/monthly', component: MonthlyHistory },
+                { path: '/edit', component: EditRecord },
+            ], meta: {requiresAuth: true}
+        },
+        {path: "/login", component: SignInPage},
+        {path: "/signUp", component: SignUpPage},
+    ],
 });
 
 router.beforeEach(async (to, from, next) => {
-  const userStore = useUserStore();
+    const userStore = useUserStore();
 
-  if (!userStore.isLoggedIn) {
-    const loginInformation = localStorage.getItem('userId');
-    if (loginInformation) {
-      await userStore.fetchUser();
+    if (!userStore.isLoggedIn) {
+        const loginInformation = localStorage.getItem("userId");
+        if (loginInformation) {
+            await userStore.fetchUser();
+        }
     }
-  }
 
-  if (to.meta.requiresAuth && !userStore.isLoggedIn) {
-    return next('/login'); //TODO: 로그인 페이지 구현
-  }
+    if (to.meta.requiresAuth && !userStore.isLoggedIn) {
+        return next("/login"); //TODO: 로그인 페이지 구현
+    }
 
-  next();
+    next();
 });
 
 export default router;
