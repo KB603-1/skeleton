ce1808684db38e81784eed75be807675f94bd5ba
SongTaeKwon <taekwon0321@gmail.com>
2026-04-10 14:12:42 +0900

    fix: API 엔드포인트 설정

--- src/plugin/api.js
+++ src/plugin/api.js
@@ -15,7 +15,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: ``,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    }
 });
 
 export default api;
