6428bbc0ee6d2e7b1b6585cfd64796750ba679b1
SongTaeKwon <taekwon0321@gmail.com>
2026-04-13 00:08:15 +0900

    fix: 화면 너비가 작을 때 날짜가 밖으로 나가는 오류 수정

--- src/components/DateTimePicker.vue
+++ src/components/DateTimePicker.vue
@@ -1,22 +1,28 @@
 <script setup>
-import { ref, watch } from 'vue';
-import { fromDate, getLocalTimeZone } from '@internationalized/date';
-import { format } from 'date-fns';
-import { Button } from '@/components/ui/button';
-import { Calendar } from '@/components/ui/calendar';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import {computed, ref, watch} from 'vue';
+import {fromDate, getLocalTimeZone} from '@internationalized/date';
+import {format} from 'date-fns';
+import {Button} from '@/components/ui/button';
+import {Calendar} from '@/components/ui/calendar';
+import {Input} from '@/components/ui/input';
+import {Label} from '@/components/ui/label';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
 
-const modelValue = defineModel({ default: () => new Date() });
+const modelValue = defineModel({default: () => new Date()});
 
 const open = ref(false);
 const date = ref(fromDate(modelValue.value, getLocalTimeZone()));
 const time = ref(format(modelValue.value, 'HH:mm:ss'));
+
+const dateTooltip = computed(() => {
+  return date ? format(date.value.toDate(getLocalTimeZone()), 'yyyy년 MM월 dd일')
+      : '날짜를 선택해주세요.'
+});
+
 
 watch([date, time], () => {
   if (!date.value) return;
@@ -34,17 +40,12 @@ watch([date, time], () => {
       <Label for="date-picker">날짜<span class="text-red-500">*</span></Label>
       <Popover v-model:open="open">
         <PopoverTrigger as-child>
-          <Button
+          <Input
               id="date-picker"
               variant="outline"
-              class="w-full justify-between font-normal"
-          >
-            {{
-              date
-                  ? format(date.toDate(getLocalTimeZone()), 'yyyy년 MM월 dd일')
-                  : '날짜를 선택해주세요.'
-            }}
-          </Button>
+              class="w-full font-normal"
+              v-model="dateTooltip"
+          />
         </PopoverTrigger>
         <PopoverContent class="w-auto overflow-hidden p-0" align="start">
           <Calendar
